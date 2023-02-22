c2243d812128ad2640d6a47e4fd2fc5c39358f35
Sean Bannon <seannbannon@gmail.com>
2023-02-22 10:18:14 -0600

    more readable now

--- static_webpage/index.js
+++ static_webpage/index.js
@@ -1,4 +1,5 @@
-ReactDOM.render(<ul><li>1.You Rock</li><li>2.You Roll</li></ul>,
-document.getElementById('root'))
+ReactDOM.render(
+    <ul><li>1.You Rock</li><li>2.You Roll</li></ul>,
+    document.getElementById('root'))
 
 
