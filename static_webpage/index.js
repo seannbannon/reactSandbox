22f25bf94c7ad809af729fd31bb572fbc34ab46c
Sean Bannon <seannbannon@gmail.com>
2023-02-23 07:18:15 -0600

    adding an imperative programming example

--- static_webpage/index.js
+++ static_webpage/index.js
@@ -2,11 +2,16 @@
 //     <ul><li>1.You Rock</li><li>2.You Roll</li></ul>,
 //     document.getElementById('root'))
 
+// imperative programming example (React is declarative)
+const h1 = document.createElement("h1")
+h1.textContent = "This is an imperative way to program."
+h1.className = "header"
+document.getElementById("root").append(h1)
+
 
 function MainContent() {
     return <h1>I am learning React!</h1>
 }
-
 
 ReactDOM.render(
     <div>
