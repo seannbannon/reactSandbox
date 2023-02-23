1f0503fb4dfb868ead88c82b4b6fca7c68cba45f
Sean Bannon <seannbannon@gmail.com>
2023-02-23 07:07:12 -0600

    making a React component and rendering it on screen

--- static_webpage/index.js
+++ static_webpage/index.js
@@ -1,5 +1,16 @@
+// ReactDOM.render(
+//     <ul><li>1.You Rock</li><li>2.You Roll</li></ul>,
+//     document.getElementById('root'))
+
+
+function MainContent() {
+    return <h1>I am learning React!</h1>
+}
+
+
 ReactDOM.render(
-    <ul><li>1.You Rock</li><li>2.You Roll</li></ul>,
-    document.getElementById('root'))
-
-
+    <div>
+        <MainContent />
+    </div>,
+    document.getElementById('root')
+)
