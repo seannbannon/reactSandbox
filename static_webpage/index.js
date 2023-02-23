e702a05ee3ff12bb188f57d234896b0a2bfa4864
Sean Bannon <seannbannon@gmail.com>
2023-02-23 07:37:52 -0600

    jsx example displaying silly navbar thing that is ugly

--- static_webpage/index.js
+++ static_webpage/index.js
@@ -1,21 +1,38 @@
+//Rendering React Code, simple list syntax
 // ReactDOM.render(
 //     <ul><li>1.You Rock</li><li>2.You Roll</li></ul>,
 //     document.getElementById('root'))
 
 // imperative programming example (React is declarative)
-const h1 = document.createElement("h1")
-h1.textContent = "This is an imperative way to program."
-h1.className = "header"
-document.getElementById("root").append(h1)
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is an imperative way to program."
+// h1.className = "header"
+// document.getElementById("root").append(h1)
 
 
-function MainContent() {
-    return <h1>I am learning React!</h1>
-}
+// function MainContent() {
+//     return <h1>I am learning React!</h1>
+// }
+// ReactDOM.render(
+//     <div>
+//         <MainContent />
+//     </div>,
+//     document.getElementById('root')
+// )
 
+
+//JSX is basically HTML in React code 
+const navBar = (
+        <nav>
+            <h1>Sean's Website</h1>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+)
 ReactDOM.render(
-    <div>
-        <MainContent />
-    </div>,
+    navBar,
     document.getElementById('root')
 )
