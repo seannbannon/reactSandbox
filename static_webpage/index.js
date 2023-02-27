4f300f557bbe4dd424ccff649b59b342e98a7d5d
Sean Bannon <seannbannon@gmail.com>
2023-02-26 21:13:02 -0600

    adding facts

--- static_webpage/index.js
+++ static_webpage/index.js
@@ -63,6 +63,13 @@ const page = (
     <div>
         <img src="./reactlogo.png" width="40px" />
         <h1>Fun Facts About React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
     </div>
 )
 
