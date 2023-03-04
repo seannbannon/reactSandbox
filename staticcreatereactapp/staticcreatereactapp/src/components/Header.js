bbb6552ce8b23bcff47f5b38b93382605f592a06
Sean Bannon <seannbannon@gmail.com>
2023-03-03 23:01:44 -0600

    adding header to app

--- staticcreatereactapp/staticcreatereactapp/src/components/Header.js
+++ staticcreatereactapp/staticcreatereactapp/src/components/Header.js
@@ -1,1 +1,16 @@
 import React from "react"
+
+export default function Header(){
+    return (
+        <header>
+        <nav className="nav">
+            <img className = "navimg" src="./reactlogo.png" />
+            <ul className = "nav-items">
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
--- staticcreatereactapp/staticcreatereactapp/src/index.js
+++ staticcreatereactapp/staticcreatereactapp/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
     return(
-    <Footer />
+    <div>
+        <Header />
+        <Footer />
+    </div>
     )
 }
 
