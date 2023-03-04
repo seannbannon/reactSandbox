82ae5979e8dcae431692b0f297a7e2a1fecc889e
Sean Bannon <seannbannon@gmail.com>
2023-03-03 23:09:05 -0600

    adding css

--- staticcreatereactapp/staticcreatereactapp/src/index.js
+++ staticcreatereactapp/staticcreatereactapp/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM  from "react-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import "./style.css"
 
 function App() {
     return(
