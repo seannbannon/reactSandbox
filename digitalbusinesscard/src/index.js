d99279d0ad413304a0bf9d5519a12decd6492249
Sean Bannon <seannbannon@gmail.com>
2023-03-05 20:50:07 -0600

    test works

---
+++ digitalbusinesscard/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Body from "./Body";
+
+function App(){
+    return(
+        <div>
+            <Body />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
