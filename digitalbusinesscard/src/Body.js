45e4f1b8ebd6a7823bddb15878b8cf9f5696efb5
Sean Bannon <seannbannon@gmail.com>
2023-03-06 23:53:03 -0600

    secret

--- digitalbusinesscard/src/Body.js
+++ digitalbusinesscard/src/Body.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 export default function Body(){
     return(
-        <div>
+        <div className='bodytext'>
+            <h1>YOU FOUND THE MESSAGE</h1>
+            <p>Hello Reader. You have found the secret message. Email me at seannbannon@flash.net to claim your prize. The secret word is FLAPJACKS.</p>
         </div>
     )
 }
--- digitalbusinesscard/src/Header.js
+++ digitalbusinesscard/src/Header.js
@@ -3,8 +3,11 @@ import React from "react";
 export default function Header(){
     return(
         <div className="header">
-            <img className="mypic" src='./me.png' />
+            <img className="mypic" src='./me.png' alt="me" />
             <h1>Sean Bannon</h1>
+            <h4>Full Stack Developer</h4>
+            {/* MAKE THE BUTTON TSAKE U TO LINKEDIN */}
+            <button type="submit" className="buttons">HELLO</button>
         </div>
     )
 }
