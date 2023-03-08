666e291ea0ceed737e7f50768cace83c117308b4
Sean Bannon <seannbannon@gmail.com>
2023-03-07 20:07:03 -0600

    centered secret word

--- digitalbusinesscard/src/Body.js
+++ digitalbusinesscard/src/Body.js
@@ -3,8 +3,9 @@ import React from 'react';
 export default function Body(){
     return(
         <div className='bodytext'>
-            <h2>YOU FOUND THE MESSAGE</h2>
-            <p>Hello Reader. You have found the secret message. Email me at seannbannon@flash.net to claim your prize. The secret word is <span className='flapjacks'>FLAPJACKS</span>.</p>
+            <h3>YOU FOUND THE MESSAGE</h3>
+            <p>Hello Reader. You have found the secret message. Email me at seannbannon@flash.net to claim your prize. The secret word is 
+            <br /><span className='flapjacks'><center>FLAPJACKS</center></span></p>
         </div>
     )
 }
