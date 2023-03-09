aaac1176df7edc0868f9c15449268da2b475e5db
Sean Bannon <seannbannon@gmail.com>
2023-03-08 20:34:04 -0600

    adding footer background

--- digitalbusinesscard/src/Footer.js
+++ digitalbusinesscard/src/Footer.js
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Footer(){
         return(
+            <div className="footer">
             <div className="icons">
-                <center>
                 <a href="https://www.linkedin.com/in/seanbannon1/" class="fa fa-linkedin"></a>
                 <a href="https://github.com/seannbannon" class="fa fa-github"></a>
                 <a href="https://www.youtube.com/channel/UC84j07Ius8soQ8QlUV-ckOQ" class="fa fa-youtube"></a>
-                <a href="https://www.linkedin.com/in/seanbannon1/" class="fa fa-linkedin"></a>
-
-                </center>
+                <a href="https://www.linkedin.com/in/seanbannon1/" class="fa fa-facebook"></a>
             </div>
+            </div>
+
         )
 }
