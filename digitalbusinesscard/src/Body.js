428c1b62126e391ce90193778eb19c03a0bdf46e
Sean Bannon <seannbannon@gmail.com>
2023-03-08 20:40:20 -0600

    adding a stupid nother paragraph

--- digitalbusinesscard/src/Body.js
+++ digitalbusinesscard/src/Body.js
@@ -6,6 +6,10 @@ export default function Body(){
             <h3>YOU FOUND THE SECRET MESSAGE</h3>
             <p>Hello Reader. You have found the secret message. Email me at seannbannon@flash.net to claim your prize. The secret word is 
             <br /><span className='flapjacks'><center>FLAPJACKS</center></span></p>
+            <br></br>
+            <h3>I GOT MAD SKILLZ</h3>
+            <p>I mean, check out this 'p tag'. You like that? I bet you do.</p>
         </div>
+
     )
 }
