81de948b1f1b27f74ba0e6a3103527b1a750a187
Sean Bannon <seannbannon@gmail.com>
2023-03-08 20:43:29 -0600

    adding components folder

--- digitalbusinesscard/src/index.js
+++ digitalbusinesscard/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css"
-import Header from "./Header";
-import Body from "./Body";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 function App(){
     return(
