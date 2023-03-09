bdff408cbdca1944dbbdbc44b4ef77ad1c21cd3a
Sean Bannon <seannbannon@gmail.com>
2023-03-08 20:03:54 -0600

    adding footer icons

--- digitalbusinesscard/src/Body.js
+++ digitalbusinesscard/src/Body.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Body(){
     return(
         <div className='bodytext'>
-            <h3>YOU FOUND THE MESSAGE</h3>
+            <h3>YOU FOUND THE SECRET MESSAGE</h3>
             <p>Hello Reader. You have found the secret message. Email me at seannbannon@flash.net to claim your prize. The secret word is 
             <br /><span className='flapjacks'><center>FLAPJACKS</center></span></p>
         </div>
--- digitalbusinesscard/src/Footer.js
+++ digitalbusinesscard/src/Footer.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 export default function Footer(){
-    <div>
         return(
             <div className="icons">
-                <h1>HELLOO!!</h1>
+                <center>
+                <a href="https://www.linkedin.com/in/seanbannon1/" class="fa fa-linkedin"></a>
+                <a href="https://github.com/seannbannon" class="fa fa-github"></a>
+                <a href="https://www.youtube.com/channel/UC84j07Ius8soQ8QlUV-ckOQ" class="fa fa-youtube"></a>
+                <a href="https://www.linkedin.com/in/seanbannon1/" class="fa fa-linkedin"></a>
+
+                </center>
             </div>
         )
-    </div>
 }
