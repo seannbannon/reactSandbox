ca5955a063e6ba33a162e270bf0c4316b20aac00
Sean Bannon <seannbannon@gmail.com>
2023-03-08 20:35:26 -0600

    adding space between icons

--- digitalbusinesscard/src/Footer.js
+++ digitalbusinesscard/src/Footer.js
@@ -4,9 +4,9 @@ export default function Footer(){
         return(
             <div className="footer">
             <div className="icons">
-                <a href="https://www.linkedin.com/in/seanbannon1/" class="fa fa-linkedin"></a>
-                <a href="https://github.com/seannbannon" class="fa fa-github"></a>
-                <a href="https://www.youtube.com/channel/UC84j07Ius8soQ8QlUV-ckOQ" class="fa fa-youtube"></a>
+                <a href="https://www.linkedin.com/in/seanbannon1/" class="fa fa-linkedin"></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="https://github.com/seannbannon" class="fa fa-github"></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="https://www.youtube.com/channel/UC84j07Ius8soQ8QlUV-ckOQ" class="fa fa-youtube"></a>&nbsp;&nbsp;&nbsp;&nbsp;
                 <a href="https://www.linkedin.com/in/seanbannon1/" class="fa fa-facebook"></a>
             </div>
             </div>
