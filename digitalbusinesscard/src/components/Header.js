b5a4c1447aebc5c63cff0307415a09a81b537019
Sean Bannon <seannbannon@gmail.com>
2023-03-09 07:26:48 -0600

    adding space between buttns

--- digitalbusinesscard/src/components/Header.js
+++ digitalbusinesscard/src/components/Header.js
@@ -7,7 +7,6 @@ export default function Header(){
             <img className="mypic" src='./me.png' alt="me" />
             <h1>Sean Bannon</h1>
             <h4>Full Stack Developer</h4>
-            {/* MAKE THE BUTTON TSAKE U TO LINKEDIN */}
             <a href = 'https://www.linkedin.com/in/seanbannon1/'>
                 <button type="submit" className="buttons"><i class="fa fa-linkedin-square"></i>LinkedIn</button>
             </a>
