3b7997af5bb780f07dbd175ed39a2009bb939723
Sean Bannon <seannbannon@gmail.com>
2023-03-10 23:16:45 -0600

    css nav bar lookin good

--- airbnbclone/src/App.js
+++ airbnbclone/src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Nav from "./components/Nav"
+import "./style.css"
 
 export default function App() {
     return (
--- airbnbclone/src/components/Nav.js
+++ airbnbclone/src/components/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Nav(){
     return(
-        <nav className="nav">
+        <nav className>
             <img className="airbnblogo" alt= "airbnblogo" src={require("../images/airbnb-logo.png")}/>
         </nav>
     )
