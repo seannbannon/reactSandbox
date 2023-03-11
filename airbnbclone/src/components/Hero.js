e17f895d59d9943b8277a999f70c71c915ddfbf4
Sean Bannon <seannbannon@gmail.com>
2023-03-11 00:11:14 -0600

    changing css style to center photo grid to be more simplistic

--- airbnbclone/src/components/Hero.js
+++ airbnbclone/src/components/Hero.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Hero(){
     return(
-        <div>
+        <section className="herodiv">
             <img className="photogrid" alt= "pics" src={require("../images/photo-grid.png")} />
-        </div>
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts-- all without leaving home.</p>
+        </section>
     )
 }
