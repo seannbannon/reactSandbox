70a655c21a0d154a346d78050bb205064309e07f
Sean Bannon <seannbannon@gmail.com>
2023-03-11 00:53:44 -0600

    katie zaferes image is displayed on bottom of page

--- airbnbclone/src/components/Card.js
+++ airbnbclone/src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Card(){
     return(
         <div>
-            
+            <img className="katiez" alt="katiez" src={require("../images/katiezaferes.png")} />
         </div>
     )
 }
