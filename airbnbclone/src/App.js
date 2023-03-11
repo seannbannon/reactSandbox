bc8955b06cb2034b66690bf5b348dcc13f0ddd6b
Sean Bannon <seannbannon@gmail.com>
2023-03-10 23:36:12 -0600

    hero collage pic is there

--- airbnbclone/src/App.js
+++ airbnbclone/src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Nav from "./components/Nav"
+import Hero from "./components/Hero"
 import "./style.css"
 
 export default function App() {
     return (
         <div>
             <Nav />
+            <Hero />
         </div>
     )
 }
---
+++ airbnbclone/src/components/Hero.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Hero(){
+    return(
+        <div>
+            <img className="photogrid" alt= "pics" src={require("../images/photo-grid.png")} />
+        </div>
+    )
+}
