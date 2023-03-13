40ff2c6fed50208878573c3a60c9a455d195d4b0
Sean Bannon <seannbannon@gmail.com>
2023-03-13 09:55:46 -0500

    some css changes

--- airbnbclone/src/components/Card.js
+++ airbnbclone/src/components/Card.js
@@ -3,8 +3,10 @@ import React from "react";
 export default function Card(){
     return(
         <div>
-            <img className="katiez" alt="katiez" src={require("../images/katiezaferes.png")} />
-            <img className="star" alt="star" src={require("../images/star.png")} />
+            <div className="pics">
+                <img className="katiez" alt="katiez" src={require("../images/katiezaferes.png")} />
+                <img className="star" alt="star" src={require("../images/star.png")} />
+            </div>
         </div>
     )
 }
