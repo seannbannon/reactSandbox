d2e824847db3094af76b3a16f1dfa178aeb271fe
Sean Bannon <seannbannon@gmail.com>
2023-03-15 22:11:22 -0500

    text

--- airbnbclone/src/components/Card.js
+++ airbnbclone/src/components/Card.js
@@ -6,6 +6,7 @@ export default function Card(){
             <div className="pics">
                 <img className="katiez" alt="katiez" src={require("../images/katiezaferes.png")} />
                 <img className="star" alt="star" src={require("../images/star.png")} />
+                <p>Life lessons with Katie Zaferes</p>
             </div>
         </div>
     )
