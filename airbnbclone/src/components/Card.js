def3e2e29b571c202606db3dac2322ef788c3f76
Sean Bannon <seannbannon@gmail.com>
2023-03-22 07:38:47 -0500

    reworking the stats so they are in line

--- airbnbclone/src/components/Card.js
+++ airbnbclone/src/components/Card.js
@@ -2,12 +2,14 @@ import React from "react";
 
 export default function Card(){
     return(
-        <div>
-            <div className="pics">
+        <div className="card">
                 <img className="katiez" alt="katiez" src={require("../images/katiezaferes.png")} />
-                <img className="star" alt="star" src={require("../images/star.png")} />
-                <p>Life lessons with Katie Zaferes</p>
-            </div>
+                <div className="cardstats">
+                    <img className="star" alt="star" src={require("../images/star.png")} />
+                    <span>5.0</span>
+                    <span>(6) • </span>
+                    <span>USA</span>
+                </div>
         </div>
     )
 }
