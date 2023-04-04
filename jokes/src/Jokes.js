3ccbf082e0d54a7cfebf3db8e0c26b9e91b5935e
Sean Bannon <seannbannon@gmail.com>
2023-04-03 23:51:54 -0500

    setting up basic props

--- jokes/src/Jokes.js
+++ jokes/src/Jokes.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Jokes(props) {
     return(
         <div className="joke-card">
-            
+            <h2>{props.setup}</h2>
+            <h1>{props.punchline}</h1>
         </div>
     )
 }
