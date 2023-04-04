88d79e9018b1b356297f716d199939a6026ee3c1
Sean Bannon <seannbannon@gmail.com>
2023-04-04 00:06:48 -0500

    added more bad jokes

--- jokes/src/App.js
+++ jokes/src/App.js
@@ -9,6 +9,22 @@ export default function App() {
             setup="What's black and white and red all over?"
             punchline="A nun falling down the stairs!"
           />
+          <Jokes 
+            setup="Why don't pirates travel on mountain roads?"
+            punchline="Scurvy!"
+          />
+          <Jokes 
+            setup="Why do bees stay in the hive during the winter?"
+            punchline="Swarm!"
+          />
+          <Jokes 
+            setup="I got my daughter a fridge for her birthday..."
+            punchline="I can't wait to see her face light up when she opens it!"
+          />
+          <Jokes 
+            setup="What's the best part about gardening?"
+            punchline="Getting down and dirty with your hoes!"
+          />
         </div>
     )
 }
