bda28b56abe006293f1f54ad5996e68112f395c7
Sean Bannon <seannbannon@gmail.com>
2023-04-03 23:57:20 -0500

    joke shows up from props.. punchline not working tho

--- jokes/src/App.js
+++ jokes/src/App.js
@@ -1,10 +1,14 @@
 import React from "react"
 import "./index.css"
+import Jokes from "./Jokes"
 
 export default function App() {
     return (
-        <div>
-          <h1>hi</h1>
+        <div className="jokes">
+          <Jokes 
+            setup="What's black and white and red all over?"
+            punchine="A nun falling down the stairs!"
+          />
         </div>
     )
 }
