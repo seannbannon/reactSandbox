a176e2aaa7ad9d834083156e034bd67d38ad61b4
Sean Bannon <seannbannon@gmail.com>
2023-04-06 20:29:49 -0500

    replacing hard coded stuff with props

--- airbnbclone/src/components/Card.js
+++ airbnbclone/src/components/Card.js
@@ -1,17 +1,20 @@
 import React from "react";
 
-export default function Card(){
+export default function Card(props){
     return(
-        <div className="card">
-                <img className="katiez" alt="katiez" src={require("../images/katiezaferes.png")} />
-                <div className="cardstats">
-                    <img className="star" alt="star" src={require("../images/star.png")} />
-                    <span>5.0</span>
-                    <span className="gray">(6) • </span>
-                    <span className="gray">USA</span>
-                </div>
-                <p>Life Lessons with Katie Zaferes</p>
-                <p><span className="bold">From $136</span> /person</p>
-        </div>
+            <div className="card">
+                <img src={props.src} />
+            </div>
+        // <div className="card">
+        //         <img className="katiez" alt="katiez" src={require("../images/katiezaferes.png")} />
+        //         <div className="cardstats">
+        //             <img className="star" alt="star" src={require("../images/star.png")} />
+        //             <span>5.0</span>
+        //             <span className="gray">(6) • </span>
+        //             <span className="gray">USA</span>
+        //         </div>
+        //         <p>Life Lessons with Katie Zaferes</p>
+        //         <p><span className="bold">From $136</span> /person</p>
+        // </div>
     )
 }
