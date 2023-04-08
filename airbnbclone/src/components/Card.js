aae9da8f7bac47d132f7d242263546a41febc1a4
Sean Bannon <seannbannon@gmail.com>
2023-04-07 23:14:11 -0500

    oh yeah it is lookin good now

--- airbnbclone/src/components/Card.js
+++ airbnbclone/src/components/Card.js
@@ -3,15 +3,15 @@ import React from "react";
 export default function Card(props){
     return(
             <div className="card">
-                <img className ="katiez" src={require(`../images/${props.mainphoto}`)} />
+                <img className ="katiez" alt="katiez" src={require(`../images/${props.mainphoto}`)} />
                 <div className="cardstats">
                     <img className="star" alt="star" src={require("../images/star.png")} />
                     <span>{props.rating}</span>
-                    <span className="gray">"({props.numberofratings})"</span>
+                    <span className="gray">({props.numberofratings}) • </span>
                     <span className="gray">{props.country}</span>
                 </div>
                 <p>{props.title}</p>
-                <p><span className="bold">{props.price}</span></p>
+                <p><span className="bold">From ${props.price}</span> / person</p>
             </div>
         // <div className="card">
         //         <img className="katiez" alt="katiez" src={require("../images/katiezaferes.png")} />
