00dfff3b6e6cfd68316fe707c85ee909e9437e9b
Sean Bannon <seannbannon@gmail.com>
2023-04-08 21:25:08 -0500

    challenge one dunn

--- challenges/index.js
+++ challenges/index.js
@@ -1,3 +1,11 @@
 // Challenge 1:
 // Given an array of numbers, return an array of each number, squared
 
+const nums = [1, 2, 3, 4, 5]
+// --> [1, 4, 9, 16, 25]
+
+const squared = nums.map(function(number) {
+    return item * item
+})
+
+console.log(squared)
