03f933d761925546d76efa23fa79c76d96c61423
Sean Bannon <seannbannon@gmail.com>
2023-04-08 22:05:46 -0500

    challenge 3 A+

--- challenges/index.js
+++ challenges/index.js
@@ -8,8 +8,8 @@ const nums = [1, 2, 3, 4, 5]
 const squared = nums.map(function(number) {
     return number * number
 })
-
 console.log(squared)
+
 
 //Challenge 2:
 //Given an array of strings, return an array where the first letter of each string is capitalized
@@ -20,5 +20,14 @@ const names = ["alice", "bob", "charlie", "butkus"]
 const capital = names.map((name) => {
     return name[0].toUpperCase() + name.slice(1)
 })
-
 console.log(capital)
+
+
+//Challenge 3:
+//Given an array of strings, return an array of strings that wraps each item of the array in an HTML like <p> tag.
+
+const pokemon = ["Mewtwo", "Pikachu", "Jigglypuff"]
+// --> ["<p>Mewtwo</p>, <p>Pikachu</p>, <p>Jigglypuff</p>"]
+
+const elements = pokemon.map(mon => `<p>${mon}</p>`)
+console.log(elements)
