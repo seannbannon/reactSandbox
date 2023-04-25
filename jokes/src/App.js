ec3fed7ba1c1facf6bed4c9428630eed7204737d
Sean Bannon <seannbannon@gmail.com>
2023-04-25 14:57:32 -0500

    making jokes data file

--- jokes/src/App.js
+++ jokes/src/App.js
@@ -5,27 +5,28 @@ import Jokes from "./Jokes"
 export default function App() {
     return (
         <div className="jokes">
-          <Jokes 
-            setup="What's black and white and red all over?"
-            punchline="A nun falling down the stairs!"
-          />
-          <Jokes 
-            setup="Why don't pirates travel on mountain roads?"
-            punchline="Scurvy!"
-          />
-          <Jokes 
-            setup="Why do bees stay in the hive during the winter?"
-            punchline="Swarm!"
-          />
-          <Jokes 
-            setup="I got my daughter a fridge for her birthday..."
-            punchline="I can't wait to see her face light up when she opens it!"
-          />
-          <Jokes 
-            setup="What's the best part about gardening?"
-            punchline="Getting down and dirty with your hoes!"
-          />
+        
         </div>
     )
 }
 
+// <Jokes 
+// setup="What's black and white and red all over?"
+// punchline="A nun falling down the stairs!"
+// />
+// <Jokes 
+// setup="Why don't pirates travel on mountain roads?"
+// punchline="Scurvy!"
+// />
+// <Jokes 
+// setup="Why do bees stay in the hive during the winter?"
+// punchline="Swarm!"
+// />
+// <Jokes 
+// setup="I got my daughter a fridge for her birthday..."
+// punchline="I can't wait to see her face light up when she opens it!"
+// />
+// <Jokes 
+// setup="What's the best part about gardening?"
+// punchline="Getting down and dirty with your hoes!"
+// />
---
+++ jokes/src/jokesData.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        setup: "What's black and white and red all over?",
+        punchline: "A nun falling down the stairs!"
+    },
+    {
+        setup: "Why don't pirates travel on mountain roads?",
+        punchline: "Scurvy!"
+    },
+    {
+        setup:"Why do bees stay in the hive during the winter?",
+        punchline:"Swarm!"
+    },
+    { 
+        setup:"I got my daughter a fridge for her birthday...",
+        punchline:"I can't wait to see her face light up when she opens it!"
+    },
+    { 
+        setup:"What's the best part about gardening?",
+        punchline:"Getting down and dirty with your hoes!"
+    }
+]
