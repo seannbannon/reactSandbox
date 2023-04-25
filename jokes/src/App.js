d79e0f479ecdb6aeab3f633dd094ff566e073e35
Sean Bannon <seannbannon@gmail.com>
2023-04-25 15:03:35 -0500

    importing joke data to app.js and mapping through each item in the array to make the code a bit cleaner and display through the jokeElements

--- jokes/src/App.js
+++ jokes/src/App.js
@@ -1,32 +1,17 @@
 import React from "react"
 import "./index.css"
 import Jokes from "./Jokes"
+import jokesData from "./jokesData"
 
 export default function App() {
+    const jokeElements = jokesData.map(joke => {
+        return <Jokes setup={joke.setup} punchline={joke.punchline} />
+    })
     return (
         <div className="jokes">
-        
+            {jokeElements}
         </div>
     )
 }
 
-// <Jokes 
-// setup="What's black and white and red all over?"
-// punchline="A nun falling down the stairs!"
-// />
-// <Jokes 
-// setup="Why don't pirates travel on mountain roads?"
-// punchline="Scurvy!"
-// />
-// <Jokes 
-// setup="Why do bees stay in the hive during the winter?"
-// punchline="Swarm!"
-// />
-// <Jokes 
-// setup="I got my daughter a fridge for her birthday..."
-// punchline="I can't wait to see her face light up when she opens it!"
-// />
-// <Jokes 
-// setup="What's the best part about gardening?"
-// punchline="Getting down and dirty with your hoes!"
-// />
+
